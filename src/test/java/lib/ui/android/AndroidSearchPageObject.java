8731e8a554ee65eecd3b6b70159e0c8135f92a3d
vitaly <vitaly.zubentsov@ya.ru>
2019-05-10 13:16:51 +0300

    8 урок. Выполнено задание из 7го урока. Произведенна правка для локатора платфомы android

--- src/test/java/lib/ui/android/AndroidSearchPageObject.java
+++ src/test/java/lib/ui/android/AndroidSearchPageObject.java
@@ -9,8 +9,8 @@ public class AndroidSearchPageObject extends SearchPageObject {
         SEARCH_INIT_ELEMENT = "xpath://*[contains(@text,'Search Wikipedia')]";
         SEARCH_INPUT = "xpath://*[contains(@text,'Search…')]";
         SEARCH_CANCEL_BUTTON = "id:org.wikipedia:id/search_close_btn";
-        SEARCH_RESULT_BY_SUBSTRING_TITLE_TPL = "xpath://*[@resource-id='org.wikipedia:id/page_list_item_container']//*[contains(@text='{SUBSTRING}')]";
-        SEARCH_RESULT_BY_SUBSTRINGS_TITLE_AND_DESCRIPTION_TPL = "xpath://*[@resource-id='org.wikipedia:id/page_list_item_container']//*[contains(@text='{TITLE}')]//..//*[contains(@text='{DESCRIPTION}')]";
+        SEARCH_RESULT_BY_SUBSTRING_TITLE_TPL = "xpath://*[@resource-id='org.wikipedia:id/page_list_item_container']//*[contains(@text,'{SUBSTRING}')]";
+        SEARCH_RESULT_BY_SUBSTRINGS_TITLE_AND_DESCRIPTION_TPL = "xpath://*[@resource-id='org.wikipedia:id/page_list_item_container']//*[contains(@text,'{TITLE}')]//..//*[contains(@text,'{DESCRIPTION}')]";
         SEARCH_RESULT_ELEMENT = "xpath://*[@resource-id='org.wikipedia:id/search_results_container']//*[@resource-id='org.wikipedia:id/page_list_item_container']";
         SEARCH_RESULT_ELEMENTS = "xpath://*[@resource-id='org.wikipedia:id/page_list_item_title']";
         SEARCH_EMPTY_RESULT_ELEMENT = "xpath://*[@text='No results found']";
